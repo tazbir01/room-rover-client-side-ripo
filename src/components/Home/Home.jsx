225054b7624b9ffd14f84d3cc842fcc83b026deb
tazbir01 <tajberahamad@gmail.com>
2023-11-04 21:13:06 +0600

    creat envirronment for roomrover project

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h2>this is</h2>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Home from './components/Home/Home';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+    children:[
+      
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
