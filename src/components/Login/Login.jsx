a284b5d42e8d2ba34744b569c127ca1a4fb04d92
tazbir01 <tajberahamad@gmail.com>
2023-11-07 16:20:26 +0600

    added login user in the login form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,22 +1,46 @@
+import { useContext, useState} from "react";
 import { Link } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
+import { authContext } from "../Provider/AuthProvider";
 
 const Login = () => {
+    const {loginUser} = useContext(authContext)
+    const [errorMessage, setErrorMessage] = useState('')
+
+    const handleLogin = e =>{
+        e.preventDefault()
+        const email = e.target.email.value
+        const password = e.target.password.value
+        console.log(email, password)
+
+        loginUser(email, password)
+        .then(result =>{
+            console.log(result.user)
+        })
+        .catch(error=>{
+            console.log(error.message)
+            setErrorMessage(error.message)
+        })
+    }
+
     return (
         <div className="text-center">
             <div className="hero min-h-screen bg-base-200">
                 {/* <div className="hero-content flex-col"> */}
                 <div className="card md:w-1/3 lg:w-1/3 ">
                     <h2 className="text-4xl font-bold">Sign in or Login</h2>
-                    <form className="card-body">
+                    <form onSubmit={handleLogin} className="card-body">
                         <button className="btn bg-blue-500 text-white"><FcGoogle className="text-2xl bg-white rounded-lg"></FcGoogle>Sign in with Google</button>
                         <p>or</p>
                         <div className="form-control">
-                            <input type="email" placeholder="email" className="input input-bordered" required />
+                            <input name="email" type="email" placeholder="email" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
-                            <input type="password" placeholder="password" className="input input-bordered" required />
+                            <input name="password" type="password" placeholder="password" className="input input-bordered" required />
                         </div>
+                        {
+                            errorMessage && <p className="text-red-500">*{errorMessage}</p>
+                        }
                         <div className="form-control mt-6">
                             <button className="btn bg-blue-500 text-white">Login</button>
                         </div>
